84c287f42fbaf5edd5b10f6bc8a8ad8bd35ccea1
ikcilrep <frajerzycki@protonmail.com>
2020-07-03 22:17:13 +0200

    Add missing user entity

---
+++ Mystik/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mystik.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
