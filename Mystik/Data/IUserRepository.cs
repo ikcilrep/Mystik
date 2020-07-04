ca54a05ec264ce9ad8731e090da8f06a183f750d
ikcilrep <frajerzycki@protonmail.com>
2020-07-04 23:10:44 +0200

    Make methods async

--- Mystik/Data/IUserRepository.cs
+++ Mystik/Data/IUserRepository.cs
@@ -1,7 +1,14 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Mystik.Entities;
+
 namespace Mystik.Data
 {
     public interface IUserRepository
     {
+        Task<User> Create(string username, string password);
+        Task<User> Retrieve(Guid id);
+        Task Update(Guid id, User user);
+        Task Delete(Guid id);
     }
 }
