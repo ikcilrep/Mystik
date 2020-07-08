ec430f8ac7697132fe5d4a4f80e4d816f8f606da
ikcilrep <frajerzycki@protonmail.com>
2020-07-08 18:38:06 +0200

    Create helper for controller authorization

---
+++ Tests/Helpers/ControllerTestExtension.cs
@@ -0,0 +1,31 @@
+namespace Tests.Helpers
+{
+    public static class ControllerTestExtensions
+    {
+        public static T WithIdentity<T>(this T controller, string id, string role) where T : Controller
+        {
+            controller.EnsureHttpContext();
+
+            var principal = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+                            {
+                                new Claim(ClaimTypes.NameIdentifier, id),
+                                new Claim(ClaimTypes.Role, role)
+                            }, "TestAuthentication"));
+
+            controller.ControllerContext.HttpContext.User = principal;
+
+            return controller;
+        }
+
+        public static T WithAnonymousIdentity<T>(this T controller) where T : Controller
+        {
+            controller.EnsureHttpContext();
+
+            var principal = new ClaimsPrincipal(new ClaimsIdentity());
+
+            controller.ControllerContext.HttpContext.User = principal;
+
+            return controller;
+        }
+    }
+}
