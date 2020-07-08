c770b05e9ffcffd5f85fa861638d0631a56dec74
ikcilrep <frajerzycki@protonmail.com>
2020-07-08 22:55:51 +0200

    Create GetById as authorized user test

--- Tests/UsersControllerTest.cs
+++ Tests/UsersControllerTest.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Mystik.Controllers;
 using Mystik.Entities;
 using Tests.Helpers;
@@ -17,6 +19,24 @@ namespace Tests
             var controller = new UsersController(service);
             var result = await controller.Get();
             Assert.True(service.Users.SetEquals(result.ToHashSet()));
+        }
+
+        [Fact]
+        public async Task GetById_AsAuthorizedUser_ReturnsCorrectUser()
+        {
+            var service = new MockUserService();
+            var id = Guid.Parse("4192105b-3256-40e2-9efb-eef265e5eaa6");
+            var controller = new UsersController(service).WithIdentity(
+                id.ToString(),
+                Role.User);
+
+
+            var expectedUser = await service.Retrieve(id);
+            var response = await controller.Get(id);
+
+            Assert.IsType<OkObjectResult>(response);
+            var ok = response as OkObjectResult;
+            Assert.Equal(expectedUser, ok.Value);
         }
     }
 }
