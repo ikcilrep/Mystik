e5b854e291d6e1e59262396a4beb224ddfc1a047
ikcilrep <frajerzycki@protonmail.com>
2020-07-10 20:39:56 +0200

    Create a test for Create method returned user

--- Tests/UserServiceTest.cs
+++ Tests/UserServiceTest.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Threading.Tasks;
 using Tests.Helpers;
+using Xunit;
 
 namespace Tests
 {
@@ -10,6 +12,24 @@ namespace Tests
         public UserServiceTest()
         {
             _provider = new UserServiceProvider();
+        }
+
+        [Fact]
+        public async Task Create_ReturnsCorrectUser()
+        {
+            var expectedUser = MockUserService.User1;
+
+            var actualUser = await _provider.UserService.Create(
+                expectedUser.Nickname,
+                expectedUser.Username,
+                expectedUser.Password);
+
+            actualUser.Id = expectedUser.Id;
+            actualUser.PasswordHash = expectedUser.PasswordHash;
+            actualUser.PasswordSalt = expectedUser.PasswordSalt;
+
+
+            Assert.Equal(expectedUser, actualUser);
         }
 
         public void Dispose()
