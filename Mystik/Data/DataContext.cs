90620b2f2983832bf31d3e189b74a3fef961f100
ikcilrep <frajerzycki@protonmail.com>
2020-07-11 21:36:54 +0200

    Add missing db sets

--- Mystik/Data/DataContext.cs
+++ Mystik/Data/DataContext.cs
@@ -10,6 +10,8 @@ namespace Mystik.Data
         public DbSet<User> Users { get; set; }
         public DbSet<Message> Messages { get; set; }
         public DbSet<Conversation> Conversations { get; set; }
+        public DbSet<UserConversation> UserConversations { get; set; }
+        public DbSet<ManagedConversation> ManagedConversations { get; set; }
 
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
