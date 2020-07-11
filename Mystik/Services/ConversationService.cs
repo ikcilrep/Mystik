dd13a2c57fc6eea4257dc36a49542e1de03e9e4d
ikcilrep <frajerzycki@protonmail.com>
2020-07-12 00:13:44 +0200

    Implement Create method

--- Mystik/Services/ConversationService.cs
+++ Mystik/Services/ConversationService.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Mystik.Data;
@@ -16,9 +17,22 @@ namespace Mystik.Services
             _context = context;
         }
 
-        public Task<User> Create(string name, User user)
+        public async Task<Conversation> Create(string name, Guid userId)
         {
-            throw new NotImplementedException();
+
+            var conversationId = Guid.NewGuid();
+            var managedConversation = new ManagedConversation { AdminId = userId, ConversationId = conversationId };
+            var conversation = new Conversation
+            {
+                Id = conversationId,
+                Name = name,
+                ManagedConversations = new HashSet<ManagedConversation> { managedConversation }
+            };
+            _context.Add(conversation);
+            _context.Add(managedConversation);
+
+            await _context.SaveChangesAsync();
+            return conversation;
         }
 
         public Task Delete(Guid id)
@@ -31,12 +45,12 @@ namespace Mystik.Services
             throw new NotImplementedException();
         }
 
-        public Task<IEnumerable<User>> GetAll()
+        public Task<IEnumerable<Conversation>> GetAll()
         {
             throw new NotImplementedException();
         }
 
-        public Task<User> Retrieve(Guid id)
+        public Task<Conversation> Retrieve(Guid id)
         {
             throw new NotImplementedException();
         }
