debb071a214e07a24220485b1bf27cc693fd96cf
ikcilrep <frajerzycki@protonmail.com>
2020-07-12 00:42:10 +0200

    Create IMessageService

---
+++ Mystik/Services/IMessageService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Mystik.Entities;
+using Mystik.Models;
+
+namespace Mystik.Services
+{
+    public interface IMessageService
+    {
+        Task<Message> Create(byte[] encryptedContent, Guid senderId, Guid conversationId);
+        Task<Message> Retrieve(Guid id);
+        Task Update(Guid id, MessagePatch model);
+        Task Delete(Guid id);
+        Task<IEnumerable<Message>> GetAll();
+
+        void Dispose();
+    }
+}
