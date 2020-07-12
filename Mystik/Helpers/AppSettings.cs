019b1f8beb7c14aee78ba42281cd164aac29702d
ikcilrep <frajerzycki@protonmail.com>
2020-07-12 18:51:31 +0200

    Add encrypted messages path to app settings

--- Mystik/Helpers/AppSettings.cs
+++ Mystik/Helpers/AppSettings.cs
@@ -3,14 +3,19 @@ namespace Mystik.Helpers
 {
     public static class AppSettings
     {
-        private static string _variableName = "MYSTIK_SECRET";
+        private static string _secretVariableName = "MYSTIK_SECRET";
         public static string Secret
         {
-            get => Environment.GetEnvironmentVariable(_variableName);
+            get => Environment.GetEnvironmentVariable(_secretVariableName);
             set
             {
-                Environment.SetEnvironmentVariable(_variableName, value);
+                Environment.SetEnvironmentVariable(_secretVariableName, value);
             }
+        }
+
+        public static string EncryptedMessagesPath
+        {
+            get => Environment.GetEnvironmentVariable("MYSTIK_ENCRYPTED_MESSAGES_PATH");
         }
     }
 }
