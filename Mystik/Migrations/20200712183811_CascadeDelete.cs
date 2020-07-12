70d0152eb044e95ca30777d51e1de90738e58676
ikcilrep <frajerzycki@protonmail.com>
2020-07-12 20:41:58 +0200

    Add migration for cascade delete

---
+++ Mystik/Migrations/20200712183811_CascadeDelete.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mystik.Migrations
+{
+    public partial class CascadeDelete : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
