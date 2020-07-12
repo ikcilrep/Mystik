34a848e77ec953d392df6421792e0b85022f13ee
ikcilrep <frajerzycki@protonmail.com>
2020-07-12 18:52:45 +0200

    Add migration for deleting encrypted content path

---
+++ Mystik/Migrations/20200712164917_RemoveEncryptedContentPath.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mystik.Migrations
+{
+    public partial class RemoveEncryptedContentPath : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EncryptedContentPath",
+                table: "Messages");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EncryptedContentPath",
+                table: "Messages",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
