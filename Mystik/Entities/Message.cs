6f34715099d17b27db2a8eb3211cc386849a8463
ikcilrep <frajerzycki@protonmail.com>
2020-07-12 18:58:27 +0200

    Create SetEncryptedContent for Message

--- Mystik/Entities/Message.cs
+++ Mystik/Entities/Message.cs
@@ -1,5 +1,7 @@
 using System;
 using System.IO;
+using System.Threading.Tasks;
+using Mystik.Helpers;
 
 namespace Mystik.Entities
 {
@@ -11,5 +13,11 @@ namespace Mystik.Entities
         public Guid ConversationId { get; set; }
         public Conversation Conversation { get; set; }
         public DateTime SentTime { get; set; }
+
+        public async Task SetEncryptedContent(byte[] encryptedContent)
+        {
+            var path = Path.Combine(AppSettings.EncryptedMessagesPath, Id.ToString());
+            await File.WriteAllBytesAsync(path, encryptedContent);
+        }
     }
 }
