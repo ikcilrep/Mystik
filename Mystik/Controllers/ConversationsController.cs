92705198370c375dfb6afaed82001d7e4bbf541e
ikcilrep <frajerzycki@protonmail.com>
2020-07-13 00:50:46 +0200

    Add missing HttpPost attribute

--- Mystik/Controllers/ConversationsController.cs
+++ Mystik/Controllers/ConversationsController.cs
@@ -20,6 +20,8 @@ namespace Mystik.Controllers
             _conversationService = conversationService;
         }
 
+
+        [HttpPost]
         public async Task<IActionResult> Post(ConversationPost model)
         {
             var currentUserId = Guid.Parse(User.Identity.Name);
