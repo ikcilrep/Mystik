32673204e67053b80457ef10c07cba2ac4eb78bb
ikcilrep <frajerzycki@protonmail.com>
2020-07-17 22:13:37 +0200

    Create ChatHub constructor

--- Mystik/Hubs/ChatHub.cs
+++ Mystik/Hubs/ChatHub.cs
@@ -1,10 +1,17 @@
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
+using Mystik.Services;
 
 namespace Mystik.Hubs
 {
+    [Authorize]
     public class ChatHub : Hub
     {
+        private IMessageService _messageService;
 
+        public ChatHub(IMessageService messageService)
+        {
+            _messageService = messageService;
+        }
     }
 }
