be1115713718aadcbed88cfda5e671617ffafc73
ikcilrep <frajerzycki@protonmail.com>
2020-07-17 23:08:47 +0200

    Create ChatHub.SendMessage

--- Mystik/Hubs/ChatHub.cs
+++ Mystik/Hubs/ChatHub.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 using Mystik.Services;
@@ -5,13 +7,33 @@ using Mystik.Services;
 namespace Mystik.Hubs
 {
     [Authorize]
-    public class ChatHub : Hub
+    public class ChatHub : Hub<IChatClient>
     {
         private IMessageService _messageService;
+        private IConversationService _conversationService;
+        private IUserService _userService;
 
-        public ChatHub(IMessageService messageService)
+        public ChatHub(IMessageService messageService,
+                       IConversationService conversationService,
+                       IUserService userService)
         {
             _messageService = messageService;
+            _conversationService = conversationService;
+            _userService = userService;
+        }
+
+        public async Task SendMessage(byte[] encryptedContent, Guid conversationId)
+        {
+            var currentUserId = Guid.Parse(Context.User.Identity.Name);
+            var conversation = await _conversationService.Retrieve(conversationId);
+            if (conversation != null
+                && await _messageService.IsTheConversationMember(conversationId, currentUserId))
+            {
+                var message = await _messageService.Create(encryptedContent, currentUserId, conversationId);
+                var sender = await _userService.Retrieve(currentUserId);
+                await Clients.Users(conversation.Members)
+                             .ReceiveMessage(encryptedContent, message.SentTime, sender.Nickname);
+            }
         }
     }
 }
---
+++ Mystik/Hubs/IChatClient.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Threading.Tasks;
+
+namespace Mystik.Hubs
+{
+    public interface IChatClient
+    {
+        Task ReceiveMessage(byte[] encryptedContent, DateTime sentTime, string senderNickname);
+    }
+}
