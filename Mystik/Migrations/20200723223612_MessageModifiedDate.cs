5ee04eb30b1a020f316806f043a71ebb7dc72917
ikcilrep <frajerzycki@protonmail.com>
2020-07-24 00:37:36 +0200

    Create a migration for ModifiedDate in Message

---
+++ Mystik/Migrations/20200723223612_MessageModifiedDate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mystik.Migrations
+{
+    public partial class MessageModifiedDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ModifiedDate",
+                table: "Messages",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ModifiedDate",
+                table: "Messages");
+        }
+    }
+}
