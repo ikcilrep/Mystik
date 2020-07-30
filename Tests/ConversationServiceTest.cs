b772379cebe9c69ed5cdf127e8f827689cb5904e
ikcilrep <frajerzycki@protonmail.com>
2020-07-31 01:28:50 +0200

    Create an IsTheConversationManager test if true

--- Tests/ConversationServiceTest.cs
+++ Tests/ConversationServiceTest.cs
@@ -1,7 +1,17 @@
+using System.Threading.Tasks;
+using Tests.Helpers;
+using Xunit;
+
 namespace Tests
 {
     public class ConversationServiceTest : ConversationServiceProvider
     {
+        [Fact]
+        public async Task IsTheConversationManager_UserIsManager_ReturnsTrue()
+        {
+            var userIsManager = await ConversationService.IsTheConversationManager(Conversation.Id, MockUserService.Admin.Id);
 
+            Assert.True(userIsManager);
+        }
     }
 }
