ec001cdb60556ab60c7d89f30d39aabac866f011
ikcilrep <frajerzycki@protonmail.com>
2020-07-31 01:35:49 +0200

    Create a GetNotManagingMembersIds test

--- Tests/ConversationServiceTest.cs
+++ Tests/ConversationServiceTest.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Tests.Helpers;
 using Xunit;
@@ -20,6 +22,15 @@ namespace Tests
             var userIsManager = await ConversationService.IsTheConversationManager(Conversation.Id, MockUserService.User1.Id);
 
             Assert.False(userIsManager);
+        }
+
+        [Fact]
+        public async Task GetNotManagingMembersIds_ReturnsCorrectIds()
+        {
+            var expectedIds = new List<Guid> { MockUserService.User1.Id };
+            var actualIds = await ConversationService.GetNotManagingMembersIds(Conversation.Id);
+
+            Assert.Equal(expectedIds, actualIds);
         }
     }
 }
