a6476a4172a2760de7d6a49e7fd1d246b64baf21
ikcilrep <frajerzycki@protonmail.com>
2020-08-03 01:38:57 +0200

    Create a IsTheConversationMember test when true

--- Tests/MessageServiceTest.cs
+++ Tests/MessageServiceTest.cs
@@ -1,9 +1,20 @@
+using System.Threading.Tasks;
 using Mystik.Helpers;
+using Tests.Helpers;
+using Xunit;
 
 namespace Tests
 {
     public class MessageServiceTest : MessageServiceProvider
     {
+        [Fact]
+        public async Task IsTheConversationMember_UserIsTheConversationMember_ReturnsTrue()
+        {
+            var userIsTheConversationMember = await MessageService.IsTheConversationMember(
+                Conversation.Id,
+                MockUserService.Admin.Id);
 
+            Assert.True(userIsTheConversationMember);
+        }
     }
 }
