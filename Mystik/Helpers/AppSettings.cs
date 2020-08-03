c12e70d7c5ecf31a24de046e91b98be1afcc4440
ikcilrep <frajerzycki@protonmail.com>
2020-08-03 23:11:24 +0200

    Create EncryptedMessagesPath Set method

--- Mystik/Helpers/AppSettings.cs
+++ Mystik/Helpers/AppSettings.cs
@@ -4,6 +4,8 @@ namespace Mystik.Helpers
     public static class AppSettings
     {
         private static string _secretVariableName = "MYSTIK_SECRET";
+        private static string _encryptedMessagesVariableName = "MYSTIK_ENCRYPTED_MESSAGES_PATH";
+
         public static string Secret
         {
             get => Environment.GetEnvironmentVariable(_secretVariableName);
@@ -15,7 +17,11 @@ namespace Mystik.Helpers
 
         public static string EncryptedMessagesPath
         {
-            get => Environment.GetEnvironmentVariable("MYSTIK_ENCRYPTED_MESSAGES_PATH");
+            get => Environment.GetEnvironmentVariable(_encryptedMessagesVariableName);
+            set
+            {
+                Environment.SetEnvironmentVariable(_encryptedMessagesVariableName, value);
+            }
         }
     }
 }
