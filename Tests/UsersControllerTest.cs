3aa1cfef9553e008f85885a657f9d478fa702530
ikcilrep <frajerzycki@protonmail.com>
2020-08-04 23:56:53 +0200

    Create a UsersControllerTest

---
+++ Tests/UsersControllerTest.cs
@@ -0,0 +1,11 @@
+using System.Threading.Tasks;
+using Mystik.Controllers;
+using Tests.Helpers;
+using Xunit;
+
+namespace Tests
+{
+    public class UsersControllerTest : UsersControllerProvider
+    {
+    }
+}
