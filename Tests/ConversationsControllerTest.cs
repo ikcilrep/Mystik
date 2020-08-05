d4c9897bd8e0919f1ff29bfe5503e1bef8d8c8d8
ikcilrep <frajerzycki@protonmail.com>
2020-08-06 01:31:54 +0200

    Create a Get test for correctness

--- Tests/ConversationsControllerTest.cs
+++ Tests/ConversationsControllerTest.cs
@@ -1,9 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Tests.Helpers;
+using Xunit;
 
 namespace Tests
 {
     public class ConversationsControllerTest : ConversationsControllerProvider
     {
+        [Fact]
+        public async Task Get_ReturnsCorrectEntities()
+        {
+            var actualConversations = await ConversationsController.Get();
 
+            var expectedConversationsIds = new HashSet<Guid> { Conversation.Id };
+
+            var actualConversationsIds = actualConversations.Select(c => c.Id);
+
+            Assert.True(expectedConversationsIds.SetEquals(actualConversationsIds));
+        }
     }
 }
