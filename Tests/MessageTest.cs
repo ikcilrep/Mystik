00cba5c1ca682a579d8ed8104469c337c7d23f26
ikcilrep <frajerzycki@protonmail.com>
2020-08-07 19:43:10 +0200

    Create a MessageTest

---
+++ Tests/MessageTest.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using Mystik.Entities;
+using Mystik.Helpers;
+using Tests.Helpers;
+using Xunit;
+
+namespace Tests
+{
+    public class MessageTest
+    {
+
+    }
+}
