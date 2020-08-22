e16f882fe2f386f323c6b9cda3405a2c9d3416ec
ikcilrep <frajerzycki@protonmail.com>
2020-08-22 02:01:20 +0200

    Construct a new user on each get

--- Tests/Helpers/MockUserService.cs
+++ Tests/Helpers/MockUserService.cs
@@ -9,63 +9,25 @@ namespace Tests.Helpers
         public HashSet<User> Users { get; set; }
         public HashSet<CoupleOfFriends> Friends { get; set; }
 
-        private static User _admin = new User("Adamek", "Adam", "Kaczka1%3")
+        public static User Admin => new User("Adamek", "Adam", "Kaczka1%3")
         {
             Id = Guid.NewGuid(),
             Role = Role.Admin
         };
-
-        private static UserWithPassword _user1 = new UserWithPassword(
+        public static UserWithPassword User1 => new UserWithPassword(
             "Kacperek",
             "Kacper",
             "#Myszka456",
             Guid.NewGuid());
-
-
-        private static UserWithPassword _user2 = new UserWithPassword(
+        public static UserWithPassword User2 => new UserWithPassword(
             "Oliwierek",
             "Oliwier",
             "Gruszka!789",
             Guid.NewGuid());
-
-        private static UserWithPassword _notExistingUser = new UserWithPassword(
+        public static UserWithPassword NotExistingUser => new UserWithPassword(
             "Lukaszek",
             "Lukasz",
             "Jablko&101112",
             Guid.NewGuid());
-
-
-        public static User Admin => _admin;
-        public static UserWithPassword User1 => _user1;
-        public static UserWithPassword User2 => _user2;
-        public static UserWithPassword NotExistingUser => _notExistingUser;
-
-        public static void ReloadUsers()
-        {
-            _admin = new User("Adamek", "Adam", "Kaczka1%3")
-            {
-                Id = Guid.NewGuid(),
-                Role = Role.Admin
-            };
-
-            _user1 = new UserWithPassword(
-               "Kacperek",
-               "Kacper",
-               "#Myszka456",
-               Guid.NewGuid());
-
-
-            _user2 = new UserWithPassword(
-               "Oliwierek",
-               "Oliwier",
-               "Gruszka!789",
-               Guid.NewGuid());
-
-            _notExistingUser = new UserWithPassword(
-               "Lukaszek",
-               "Lukasz",
-               "Jablko&101112",
-               Guid.NewGuid());
-        }
-    }
+   }
 }
